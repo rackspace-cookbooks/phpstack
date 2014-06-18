15e5fc51df6f623feeed436ebad82b75e67878e1
Matthew Thode <mthode@mthode.org>
2014-06-18 12:50:01 -0500

    update release to catch missing ini

--- attributes/apache.rb
+++ attributes/apache.rb
@@ -19,7 +19,7 @@
 #
 
 site1 = 'example.com'
-version1 = '0.0.5'
+version1 = '0.0.6'
 
 node.default['apache']['sites'][site1]['port']         = 80
 node.default['apache']['sites'][site1]['cookbook']     = 'lampstack'
