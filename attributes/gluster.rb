d3f58c1df04f71d094abacfe81529cd2c0336d74
Matthew Thode <mthode@mthode.org>
2014-06-20 16:29:27 -0500

    glusterific

---
+++ attributes/gluster.rb
@@ -0,0 +1,12 @@
+default['rackspace_gluster']['config']['server']['glusters']['Gluster Cluster 1'] = {}
+default['rackspace_gluster']['config']['server']['glusters']['Gluster Cluster 1']['volume'] = 'vol0'
+
+#default['rackspace_gluster']['config']['server']['glusters']['Gluster Cluster 1']['nodes']['gluster01']['ip'] = '33.33.33.10'
+#default['rackspace_gluster']['config']['server']['glusters']['Gluster Cluster 1']['nodes']['gluster01']['block_device'] = '/dev/sdb'
+#default['rackspace_gluster']['config']['server']['glusters']['Gluster Cluster 1']['nodes']['gluster01']['mount_point'] = '/mnt/brick0'
+#default['rackspace_gluster']['config']['server']['glusters']['Gluster Cluster 1']['nodes']['gluster01']['brick_dir'] = '/mnt/brick0/brick'
+
+#default['rackspace_gluster']['config']['server']['glusters']['Gluster Cluster 1']['nodes']['gluster02']['ip'] = '33.33.33.11'
+#default['rackspace_gluster']['config']['server']['glusters']['Gluster Cluster 1']['nodes']['gluster02']['block_device'] = '/dev/sdb'
+#default['rackspace_gluster']['config']['server']['glusters']['Gluster Cluster 1']['nodes']['gluster02']['mount_point'] = '/mnt/brick0'
+#default['rackspace_gluster']['config']['server']['glusters']['Gluster Cluster 1']['nodes']['gluster02']['brick_dir'] = '/mnt/brick0/brick'
--- recipes/gluster.rb
+++ recipes/gluster.rb
@@ -18,4 +18,28 @@
 # limitations under the License.
 #
 
+include_recipe 'chef-sugar'
+
+cluster = node['rackspace_gluster']['config']['server']['glusters'].values[0]
+
+# set the replica count to the number of gluster nodes
+node.default['rackspace_gluster']['config']['server']['glusters'].values[0]['replica'] = cluster['nodes'].values.count
+# node.default['rackspace_gluster']['config']['server']['glusters']['Gluster Cluster 1']['replica'] = cluster['nodes'].values.count
+
+# allow application_php nodes to connect
+search_add_iptables_rules('recipe:lampstack\:\:application_php', 'INPUT', '-j ACCEPT', 70, 'web nodes access to gluster')
+
+# dynamically generate the authorized clients
+gluster_servers = search('node', 'recipe:lampstack\:\:gluster')
+gluster_ips = ['127.0.0.1']
+gluster_servers.each do |gluster_server|
+  gluster_ips.push best_ip_for(gluster_ips)
+end
+node.default['rackspace_gluster']['config']['server']['glusters']['Gluster Cluster 1']['auth_clients'] = gluster_ips.join(',')
+
+# allow the gluster nodes to connect to eachother
+cluster['nodes'].values.each do |gluster_node|
+  add_iptables_rule('INPUT', "-s #{gluster_node['ip']} -j ACCEPT", 50, 'allow gluster')
+end
+
 include_recipe 'rackspace_gluster'
