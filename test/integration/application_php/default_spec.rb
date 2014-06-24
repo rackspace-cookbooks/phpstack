2f6f731666f92009b565dfbf32880023b66de908
Hart Hoover <hart.hoover@rackspace.com>
2014-06-24 14:01:21 -0500

    Add testing for application_php suite

---
+++ test/integration/application_php/default_spec.rb
@@ -0,0 +1,21 @@
+# Encoding: utf-8
+
+require_relative 'spec_helper'
+
+if os[:family] == 'RedHat'
+  describe service('httpd') do
+    it { should be_enabled }
+  end
+else
+  describe service('apache2') do
+    it { should be_enabled }
+  end
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe file('/etc/php.ini') do
+  it { should be_file }
+end
