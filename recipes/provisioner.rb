76fd26c9e4b3ac626d3759e0cc3a2a28865ca987
Hart Hoover <hart.hoover@rackspace.com>
2014-06-24 11:01:06 -0500

    Fix Chef Metal recipe
    - add attributes for chef metal settings
    - add attributes to chef metal machine resources
    - update run lists for chef metal machine resources

--- recipes/provisioner.rb
+++ recipes/provisioner.rb
@@ -5,24 +5,11 @@
 #
 # Copyright 2014, Rackspace Hosting
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-case node['platform_family']
-when 'debian'
+case node['platform']
+when 'debian', 'ubuntu'
   node.set['apt']['compile_time_update'] = true
   include_recipe 'apt'
-when 'redhat'
+when 'redhat', 'centos', 'fedora', 'amazon', 'scientific'
   include_recipe 'yum'
 end
 
@@ -30,12 +17,12 @@ node.set['build-essential']['compile_time'] = true
 include_recipe 'build-essential'
 
 chef_gem 'chef-metal' do
-  version '0.11.2'
+  version '0.12.1'
   action 'install'
 end
 
 chef_gem 'chef-metal-fog' do
-  version '0.5.3'
+  version '0.6.1'
   action 'install'
 end
 
@@ -44,7 +31,8 @@ require 'chef_metal_fog'
 require 'cheffish'
 require 'fog'
 
-rackspace = Chef::DataBagItem.load('secrets', 'raxcloud')
+# Get credentials
+rackspace = Chef::DataBagItem.load('secrets', 'rackspacecloud')
 username = rackspace['username']
 apikey = rackspace['apikey']
 
@@ -52,37 +40,99 @@ with_driver 'fog:Rackspace:https://identity.api.rackspacecloud.com/v2.0',
             compute_options: {
               rackspace_api_key: apikey,
               rackspace_username: username,
-              rackspace_region: 'dfw'
+              rackspace_region: node['phpstack']['provisioner']['region']
             }
 
-fog_key_pair 'metal-key'
+# You need to generate a keypair in ~/.ssh for use with Metal
+fog_key_pair node['phpstack']['provisioner']['key_name']
 
 with_machine_options ssh_username: 'root',
                      bootstrap_options: {
-                       key_name: 'metal-key',
-                       flavor_id: 'performance1-2', # 2GB Performance Cloud
-                       image_id: 'ffa476b1-9b14-46bd-99a8-862d1d94eb7a' # Ubuntu 12.04
+                       key_name: node['phpstack']['provisioner']['key_name'],
+                       flavor_id: node['phpstack']['provisioner']['flavor_id'],
+                       image_id: node['phpstack']['provisioner']['image_id']
                      }
 
+# Read Chef Config from knife.rb
 with_chef_server Chef::Config[:chef_server_url],
                  client_name: Chef::Config[:node_name],
                  signing_key_filename: Chef::Config[:client_key]
 
-machine 'database' do
+machine 'phpstack-mysql-master' do
+  chef_environment '_default'
   recipe 'apt'
-  recipe 'phpstack::mysql_standalone'
-  notifies 'create', 'machine[lamp]', 'immediately'
+  recipe 'platformstack::default'
+  recipe 'rackops_rolebook::default'
+  recipe 'phpstack::mysql_master'
+  recipe 'phpstack::demo'
+  attributes(
+    platformstack: {
+      cloud_monitoring: {
+        enabled: 'true'
+      }
+    },
+    rackspace: {
+      cloudbackup: {
+        enabled: 'true'
+      },
+      cloud_credentials: {
+        username: username,
+        api_key: apikey
+      }
+    }
+  )
 end
 
-machine 'lamp' do
+machine 'phpstack-mysql-slave' do
+  chef_environment '_default'
   recipe 'apt'
-  recipe 'phpstack::lamp'
-  action 'nothing'
-  notifies 'create', 'machine[web]', 'immediately'
+  recipe 'platformstack::default'
+  recipe 'rackops_rolebook::default'
+  recipe 'phpstack::mysql_slave'
+  recipe 'phpstack::demo'
+  attributes(
+    platformstack: {
+      cloud_monitoring: {
+        enabled: 'true'
+      }
+    },
+    rackspace: {
+      cloudbackup: {
+        enabled: 'true'
+      },
+      cloud_credentials: {
+        username: username,
+        api_key: apikey
+      }
+    }
+  )
 end
 
-machine 'web' do
-  recipe 'apt'
-  recipe 'phpstack::apache'
-  action 'nothing'
+num_app = node['phpstack']['provisioner']['app_nodes']
+
+1.upto(num_app) do |i|
+  machine "phpstack-app-#{i}" do
+    chef_environment '_default'
+    recipe 'apt'
+    recipe 'platformstack::default'
+    recipe 'rackops_rolebook::default'
+    recipe 'phpstack::application_php'
+    recipe 'phpstack::demo'
+    attributes(
+      platformstack: {
+        cloud_monitoring: {
+          enabled: 'true'
+        }
+      },
+      rackspace: {
+        cloudbackup: {
+          enabled: 'true'
+        },
+        cloud_credentials: {
+          username: username,
+          api_key: apikey
+        }
+      }
+    )
+  end
 end
