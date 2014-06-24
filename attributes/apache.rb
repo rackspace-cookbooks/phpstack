0e0e3e9b7461ddd90b3cca747e3bec90f277e2d2
Matthew Thode <mthode@mthode.org>
2014-06-24 13:27:26 -0500

    some fixups to allow gluster to work with application_php
    fixes #41
    fixes #42
    
    - updated the version of the demo app to work with phpstack.ini
    - updated the demo docs to add more info on handling the gluster re-init
    - fixed some searching that gluster did for it's values

--- attributes/apache.rb
+++ attributes/apache.rb
@@ -19,7 +19,7 @@
 #
 
 site1 = 'example.com'
-version1 = '0.0.6'
+version1 = '0.0.7'
 
 node.default['apache']['sites'][site1]['port']         = 80
 node.default['apache']['sites'][site1]['cookbook']     = 'phpstack'
