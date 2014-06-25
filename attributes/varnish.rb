d2cb5b913f5624e9fa6011fbaa655251a75a1b93
Fernando Honig <fernando.honig@rackspace.co.uk>
2014-06-25 10:12:53 +0000

    Fixing rubocop and foodcritic

--- attributes/varnish.rb
+++ attributes/varnish.rb
@@ -17,3 +17,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+default['varnish']['listen_port'] = 8080
+default['varnish']['backend_port'] = 80
