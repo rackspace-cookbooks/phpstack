f22189812ef926e7b0fb84573d0eeccc9106dee0
Matthew Thode <mthode@mthode.org>
2014-08-25 14:23:08 -0500

    foodcritic upstream has errors

--- recipes/mysql_base.rb
+++ recipes/mysql_base.rb
@@ -85,8 +85,8 @@ unless includes_recipe?('phpstack::mysql_slave')
 
     # set up the default DB name, user and password
     db_name = site_name[0...64]
-    node.set_unless[node['phpstack']['webserver']]['sites'][site_name]['databases'][db_name]['mysql_user'] = site_name[0...16]
-    node.set_unless[node['phpstack']['webserver']]['sites'][site_name]['databases'][db_name]['mysql_password'] = secure_password
+    node.set_unless[node['phpstack']['webserver']]['sites'][site_name]['databases'][db_name]['mysql_user'] = site_name[0...16] # ~FC047
+    node.set_unless[node['phpstack']['webserver']]['sites'][site_name]['databases'][db_name]['mysql_password'] = secure_password # ~FC047
 
     if Chef::Config[:solo]
       Chef::Log.warn('This recipe uses search. Chef Solo does not support search.')
@@ -104,8 +104,8 @@ unless includes_recipe?('phpstack::mysql_slave')
       end
 
       # set up db user and pass for database (the non-default ones) unless set (both to random)
-      node.set_unless[node['phpstack']['webserver']]['sites'][site_name]['databases'][database]['mysql_user'] = ::SecureRandom.hex
-      node.set_unless[node['phpstack']['webserver']]['sites'][site_name]['databases'][database]['mysql_password'] = secure_password
+      node.set_unless[node['phpstack']['webserver']]['sites'][site_name]['databases'][database]['mysql_user'] = ::SecureRandom.hex # ~FC047
+      node.set_unless[node['phpstack']['webserver']]['sites'][site_name]['databases'][database]['mysql_password'] = secure_password # ~FC047
 
       app_nodes.each do |app_node|
         mysql_database_user node['apache']['sites'][site_name]['databases'][database]['mysql_user'] do
