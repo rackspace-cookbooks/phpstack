a3f9e0af891248776b293c51b03524c051cf86e0
Martin Smith <martin@mbs3.org>
2014-10-06 08:05:20 -0500

    Enable chef-zero with phpstack default/aio converge (currently fails)

--- test/unit/spec/default_spec.rb
+++ test/unit/spec/default_spec.rb
@@ -4,7 +4,8 @@ require_relative 'spec_helper'
 
 # this will pass on templatestack, fail elsewhere, forcing you to
 # write those chefspec tests you always were avoiding
-describe 'phpstack::default' do
+describe 'phpstack::default all in one demo' do
+  recipes_for_demo = ['mysql_base', 'postgresql_base', 'mongodb_standalone', 'memcache', 'varnish', 'rabbitmq', 'redis_single', 'application_php'].map{|r| "phpstack::#{r}"}
   before { stub_resources }
   supported_platforms.each do |platform, versions|
     versions.each do |version|
@@ -12,13 +13,14 @@ describe 'phpstack::default' do
         let(:chef_run) do
           ChefSpec::Runner.new(platform: platform, version: version) do |node|
             node_resources(node)
-          end.converge(described_recipe)
+            node.set['phpstack']['demo']['enabled'] = true
+          end.converge(*recipes_for_demo) # *splat operator for array to vararg
         end
 
         property = load_platform_properties(platform: platform, platform_version: version)
 
-        it 'writes some chefspec code' do
-          skip "write some tests #{property}"
+        it 'renders /etc/phpstack.ini' do
+          expect(chef_run).to create_template('/etc/phpstack.ini')
         end
       end
     end
--- test/unit/spec/spec_helper.rb
+++ test/unit/spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # Encoding: utf-8
 require 'rspec/expectations'
 require 'chefspec'
+require 'chefspec/server'
 require 'chefspec/berkshelf'
 require 'chef/application'
 require 'json'
@@ -12,10 +13,13 @@ Dir['./test/unit/spec/support/**/*.rb'].sort.each { |f| require f }
   log_level: ::LOG_LEVEL
 }
 
-def node_resources
+def node_resources(node)
+  # for chefspec, so we don't have to converge elkstack
+  node.default['elkstack']['config']['additional_logstash_templates'] = []
 end
 
 def stub_resources
+  stub_command("/usr/sbin/httpd -t").and_return(0)
 end
 
 at_exit { ChefSpec::Coverage.report! }
