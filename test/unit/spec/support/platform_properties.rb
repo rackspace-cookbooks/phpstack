49db2a864c3ec1a9dd480e38f49fefeb48cfdf52
Martin Smith <martin@mbs3.org>
2014-10-06 08:04:13 -0500

    Make unit tests runnable again

--- test/unit/spec/support/platform_properties.rb
+++ test/unit/spec/support/platform_properties.rb
@@ -3,5 +3,7 @@ require 'json'
 def load_platform_properties(args)
   platform_file_str = "../../../fixtures/platform/#{args[:platform]}/#{args[:platform_version]}.json"
   platform_file_name = File.join(File.dirname(__FILE__), platform_file_str)
-  JSON.parse(IO.read(platform_file_name), symbolize_names: true)
+  platform_str = '{}'
+  platform_str = IO.read(platform_file_name) if File.exist?(platform_file_name)
+  JSON.parse(platform_str, symbolize_names: true)
 end
