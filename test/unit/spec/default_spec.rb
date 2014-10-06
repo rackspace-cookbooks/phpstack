ac80a15265701bf7fea7d60d8db203125ee4c505
Martin Smith <martin@mbs3.org>
2014-10-06 10:08:09 -0500

    Configure chefspec tests so I could verify the phpstack.ini template works

--- test/unit/spec/default_spec.rb
+++ test/unit/spec/default_spec.rb
@@ -2,17 +2,26 @@
 
 require_relative 'spec_helper'
 
-# this will pass on templatestack, fail elsewhere, forcing you to
-# write those chefspec tests you always were avoiding
-describe 'phpstack::default all in one demo' do
-  recipes_for_demo = ['mysql_base', 'postgresql_base', 'mongodb_standalone', 'memcache', 'varnish', 'rabbitmq', 'redis_single', 'application_php'].map{|r| "phpstack::#{r}"}
+# the runlist came from test-kitchen's default suite
+describe 'phpstack all in one demo' do
+  recipes_for_demo = [
+    'mysql_base',
+    'postgresql_base',
+    'mongodb_standalone',
+    'memcache',
+    'varnish',
+    'rabbitmq',
+    'redis_single',
+    'application_php'
+    ].map{|r| "phpstack::#{r}"}
   before { stub_resources }
   supported_platforms.each do |platform, versions|
     versions.each do |version|
       context "on #{platform.capitalize} #{version}" do
         let(:chef_run) do
           ChefSpec::Runner.new(platform: platform, version: version) do |node|
-            node_resources(node)
+            node_resources(node) # stub this node
+            stub_nodes(platform, version) # stub other nodes for chef-zero
             node.set['phpstack']['demo']['enabled'] = true
           end.converge(*recipes_for_demo) # *splat operator for array to vararg
         end
@@ -21,6 +30,15 @@ describe 'phpstack::default all in one demo' do
 
         it 'renders /etc/phpstack.ini' do
           expect(chef_run).to create_template('/etc/phpstack.ini')
+          [ '[MySQL-foo]',
+            'master-host = 10.20.30.40',
+            'slave-hosts = 10.20.20.20, 10.20.20.30',
+            'port = 3306',
+            'db_name = foo',
+            'username = fooUser',
+            'password = bar' ].each do |l|
+            expect(chef_run).to render_file('/etc/phpstack.ini').with_content(/#{l}/i)
+          end
         end
       end
     end
--- test/unit/spec/spec_helper.rb
+++ test/unit/spec/spec_helper.rb
@@ -16,10 +16,23 @@ Dir['./test/unit/spec/support/**/*.rb'].sort.each { |f| require f }
 def node_resources(node)
   # for chefspec, so we don't have to converge elkstack
   node.default['elkstack']['config']['additional_logstash_templates'] = []
+
+  # for postgres cookbook, https://github.com/hw-cookbooks/postgresql#chef-solo-note
+  node.set['postgresql']['password']['postgres'] = 'chef-solo'
 end
 
 def stub_resources
   stub_command("/usr/sbin/httpd -t").and_return(0)
+  stub_command("/usr/sbin/apache2 -t").and_return(0)
+  stub_command("which php").and_return('/usr/bin/php')
+end
+
+def stub_nodes(platform, version)
+  Dir['./test/integration/nodes/*.json'].sort.each do |f|
+    node_data = JSON.parse(IO.read(f), symbolize_names: false)
+    node_name = node_data['name']
+    ChefSpec::Server.create_node(node_name, node_data)
+  end
 end
 
 at_exit { ChefSpec::Coverage.report! }
