fb89d804e18dc0622fd83c07e03667ba2722d253
bobross419 <bobross419@gmail.com>
2014-10-24 21:13:11 -0400

    ?

--- recipes/application_php.rb
+++ recipes/application_php.rb
@@ -128,7 +128,7 @@ if node.deep_fetch(stackname, 'code-deployment', 'enabled')
           restart_command
           environment_name
           enable_submodules).each do |a|
-          unless site_opts[a].nil
+          unless site_opts[a].nil?
             a site_opts[a]
           end
         end
