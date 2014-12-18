939b89dbbebbf2b1d0058059fd3f9fad60d2cf58
Matthew Thode <mthode@mthode.org>
2014-12-18 14:40:39 -0600

    updating Gemfile and removing long_description for now
    Signed-off-by: Matthew Thode <mthode@mthode.org>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ name 'phpstack'
 maintainer 'Rackspace'
 maintainer_email 'rackspace-cookbooks@rackspace.com'
 license 'Apache 2.0'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+# long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 description 'Provides a full php stack'
 version '3.0.22'
 
